d7fd0754f21cb69194dce28ecb7f8221f4c85ce0
flodrt <floriandrouot@hotmail.fr>
2024-01-09 11:47:08 +0100

    first commit

---
+++ test_calcul_unitest.py
@@ -0,0 +1,19 @@
+import unittest
+from calcul import Calcul
+
+#class de test heritant de unitest.testcase
+class TestCalcul(unittest.TestCase):
+
+    #methode de configuration a executer avant chaque test 
+    def setUp(self):
+        #initialisation des ressources necessaire pour les test 
+        self.calc=Calcul() #ibnstancie un objet calc a la class Calcul grace au self
+
+    #test d'addition
+    def test_additionner(self):
+        resultat = self.calc.additionner(3,5)
+        #test 
+        self.assertEqual(resultat,8,"commentaire spéciale")
+
+    
+
