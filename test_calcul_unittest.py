3412f401a36de564c47d7eaaf640b0bfc6b17d43
flodrt <floriandrouot@hotmail.fr>
2024-01-09 13:40:52 +0100

    fourth commit all test

--- test_calcul_unittest.py
+++ test_calcul_unittest.py
@@ -20,5 +20,30 @@ class TestCalcul(unittest.TestCase):
         #test test
         self.assertEqual(resultat,7,"commentaire spéciale")
 
-    
+    # Test de soustraction
+    def test_soustraire(self):
+        resultat = self.calc.soustraire(10, 4)
+        # Vérifier si le résultat est égal à 6
+        self.assertEqual(resultat, 6)
+
+    # Test de multiplication
+    def test_multiplier(self):
+        resultat = self.calc.multiplier(10, 4)
+        # Vérifier si le résultat est égal à 40
+        self.assertEqual(resultat, 40)
+
+    # Test de division
+    def test_diviser(self):
+        resultat = self.calc.diviser(9, 3)
+        # Vérifier si le résultat est égal à 3
+        self.assertEqual(resultat, 3)
+    # Test de division par zéro
+        
+
+        
+
+         
+# Exécuter les tests si le script est exécuté directement
+if __name__ == '__main__':# appelle la fonction main du module unittest
+    unittest.main()
 
